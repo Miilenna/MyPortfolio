c73a63091e83c2f499e5612ebeb1156bd1acb14e
Miilenna <milena200.fv@gmail.com>
2025-09-28 13:49:13 +0200

    pruebas

--- src/app/components/about-me/about-me.ts
+++ src/app/components/about-me/about-me.ts
@@ -12,13 +12,13 @@ export class AboutMe {
   constructor(private router: Router) {}
 
   verCvEs(){
-    window.open("/cv_es.pdf", '_blank');
+    window.open("assets/cv_es.pdf", '_blank');
   }
   verCvEn(){
-    window.open("/cv_en.pdf", '_blank');
+    window.open("assets/cv_en.pdf", '_blank');
   }
   verRecomendacion(){
-    window.open("/CARTA_RECOMANACIO_MILENA.pdf", '_blank');
+    window.open("assets/CARTA_RECOMANACIO_MILENA.pdf", '_blank');
   }
   return(){
    this.router.navigate(["/home.html"]) ;
